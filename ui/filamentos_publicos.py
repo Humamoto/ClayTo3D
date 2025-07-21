7de79d847fa910be7a57e301dd206aa4650a7e9d
unknown <claytonhumamoto@outlook.com>
2025-07-20 21:10:46 -0300

    Atualiza lista de filamentos públicos para calculadora do cliente

---
+++ ui/filamentos_publicos.py
@@ -0,0 +1,12 @@
+FILAMENTOS_PUBLICOS = [
+    (1, 'PLA', 'Bege Caucasiano', 'Voolt', 94.91),
+    (2, 'PLA', 'Cinza Claro', 'Voolt', 92.53),
+    (3, 'PLA', 'Rosa Salmão Neon', 'Voolt', 105.16),
+    (4, 'PLA', 'Roxo Translúcido', 'Voolt', 105.16),
+    (5, 'PLA', 'Azul Safira', 'Voolt', 115.68),
+    (6, 'PLA', 'Azul Velvet', 'Voolt', 92.53),
+    (7, 'PLA', 'Amarelo Velvet', 'Voolt', 92.53),
+    (8, 'PLA', 'Vermelho Velvet', 'Voolt', 89.21),
+    (9, 'PLA', 'Preto Velvet', 'Voolt', 89.21),
+    (10, 'PLA', 'Branco Velvet', 'Voolt', 89.21),
+]
