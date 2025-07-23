08f39866990b5f02b94cf970754fa288d7c9a184
unknown <claytonhumamoto@outlook.com>
2025-07-22 23:04:34 -0300

    botao do zap

--- ui/calculadora_cliente.py
+++ ui/calculadora_cliente.py
@@ -174,7 +174,7 @@ def pagina_calculadora_cliente():
         Para ter uma estimativa precisa, você precisa informar o **Tempo de Impressão** e o **Peso Total** da peça.
         Essas informações estão disponíveis na página de cada modelo no site [MakerWorld](https://makerworld.com/pt), dentro da seção de **"Imprimir perfil"** ou no **pop-up de detalhes** que aparece ao clicar em um perfil de impressão.
         """)
-        st.image("https://photos.google.com/u/1/photo/AF1QipOGKMjwh-a0G-3Twz7BBSFYNbVAy_1L4n9Y8bN0", caption="Clique no perfil de impressão para ver os detalhes") # Substituir por link real
+        st.image("https://github.com/Humamoto/ClayTo3D/blob/main/tuto.png?raw=true", caption="Clique no perfil de impressão para ver os detalhes")
         st.markdown("""
         **No MakerWorld, procure por:**
 
